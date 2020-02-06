67d81b77f702063ec91f21dd3f05292b13ad7ca8
AndreasHasNoHope <tsimtsiris1999@hotmail.com>
2020-02-06 14:32:27 +0200

    charts

--- Routes/Admin/admin.js
+++ Routes/Admin/admin.js
@@ -8,6 +8,28 @@ route.get("/", adminAuth, (req, res) => {
         message: 'Admin area'
     });
 });
+route.get("/stats", adminAuth, async (req, res) => {
+    const categories = await Category.find({}).exec();
+    const labels = categories.map(c => c.title);
+    const counts = [];
+    for(let cat of categories) {
+        const num =  await  Product.count({ category: cat._id});
+        counts.push(num);
+    }
+
+    res.json({
+        success: true,
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "My Categories",
+                    data: counts
+                }
+            ]
+        }
+    });
+});
 
 route.use("/auth", require("./auth"));
 route.use("/users", require('./users'));
